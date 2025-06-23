0777b5db9bb5f643d8251648c11f108521ba1e6c
harsh-1401 <159872249+harsh-1401@users.noreply.github.com>
2025-06-23 19:33:11 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
     try {
       const formData = new FormData();
       formData.append('file', file);
-      const res = await fetch('http://localhost:5000/api/analyze-call', {
+      const res = await fetch('https://feedback-ai-backend-zhkk.onrender.com', {
         method: 'POST',
         body: formData,
       });
